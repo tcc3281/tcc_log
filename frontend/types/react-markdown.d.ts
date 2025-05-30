ca24fd255f2cb5961452dbee347f56f1042f6d2b
Chiến Trần <116490257+tcc3281@users.noreply.github.com>
2025-05-30 21:28:45 +0700

    update chatbot

---
+++ frontend/types/react-markdown.d.ts
@@ -0,0 +1,8 @@
+declare module 'react-markdown/lib/ast-to-react' {
+  export interface CodeProps {
+    className?: string;
+    node?: any;
+    children?: React.ReactNode;
+    [key: string]: any;
+  }
+}
